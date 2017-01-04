3a9336fd39d3c05d71a6c72b4aa8e1d58d4f5b47
Tuomas Tiainen <tiaintu@gmail.com>
2017-01-04 12:49:59 +0200

    Change version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-inlinecss',
-    version="0.1.2",
+    version="0.2.2",
     description='A Django app useful for inlining CSS (primarily for e-mails)',
     long_description=open('README.md').read(),
     author='Philip Kimmey',
