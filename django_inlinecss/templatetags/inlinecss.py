c12107179f9e1f810aae4543e06a5c2179574b49
Tuomas Tiainen <tiaintu@gmail.com>
2017-01-04 12:44:55 +0200

    Fix trying to use path for non-local django storages

--- django_inlinecss/templatetags/inlinecss.py
+++ django_inlinecss/templatetags/inlinecss.py
@@ -22,10 +22,7 @@ class InlineCssNode(template.Node):
             path = expression.resolve(context, True)
             if path is not None:
                 path = smart_text(path)
-            if settings.DEBUG:
-                expanded_path = finders.find(path)
-            else:
-                expanded_path = staticfiles_storage.path(path)
+            expanded_path = finders.find(path)
 
             with open(expanded_path) as css_file:
                 css = ''.join((css, css_file.read()))
